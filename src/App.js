5bf06371b3b22412be57c01a9c465b24b37612d9
Emma Chory <emmachory@gmail.com>
2022-03-02 15:17:38 -0700

    Create votingpage component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import './App.css';
+import {Component} from "react"
+import { Route, Switch } from "react-router-dom";
+import './App.css'
+import VotingPage from "./VotingPage/VotingPage"
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
 
+    }
+  }
+
+  render() {
+    return(
+      <Switch>
+        <Route exact path="/">
+          <VotingPage />
+        </Route>
+      </Switch>
+    )
+  }
+}
 
 export default App;
---
+++ src/VotingPage/VotingPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const VotingPage = () => {
+  return(
+    <div>
+      <h1>Judge A Book</h1>
+    </div>
+  )
+}
+
+export default VotingPage
