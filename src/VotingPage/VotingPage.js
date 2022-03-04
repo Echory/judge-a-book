05e7abee3b07a11d84688c01d7fc15888bf55b51
Emma Chory <emmachory@gmail.com>
2022-03-04 11:11:20 -0700

    Change color scheme and add back button

--- src/VotingPage/VotingPage.js
+++ src/VotingPage/VotingPage.js
@@ -16,8 +16,8 @@ const VotingPage = ({book, nextBook, saveBook}) => {
         <section className="cover-section">
           <img className="cover-img" src={book.volumeInfo.imageLinks.thumbnail}/>
           <div className="vote-buttons">
-            <button onClick={saveBook}>👍</button>
-            <button onClick={nextBook}>👎</button>
+            <button className="vote-button" onClick={saveBook}>👍</button>
+            <button className="vote-button" onClick={nextBook}>👎</button>
           </div>
         </section>
       </div>
