07bb82a503075bdb9fef8b59e01e26968dda0112
Emma Chory <emmachory@gmail.com>
2022-03-04 13:08:58 -0700

    Start votepage cypress

---
+++ cypress/integration/votepage.js
@@ -0,0 +1,39 @@
+describe("Load voting and render the expected page elements", () => {
+  it("Should confirm that true is equal to true", () => {
+    expect(true).to.equal(true);
+  });
+  it("Should be able to visit the page and render the header", () => {
+    cy.visit("http://localhost:3000")
+      .get("div.header")
+      .should("be.visible");
+  });
+  it("Should be able to visit the page and render the 'Get More Inspiration' button", () => {
+    cy.visit("http://localhost:3000/favorites")
+      .get("div.saved-button")
+  });
+  // it("Should be able to visit the page and render the 'Your Favorite Inspirations' header", () => {
+  //   cy.visit("http://localhost:3000/favorites").contains(
+  //     "h1",
+  //     "Your Favorite Inspirations"
+  //   );
+  // });
+  // it("Should be able to visit the page and render the favorites container", () => {
+  //   cy.visit("http://localhost:3000/favorites").get("div.favorite");
+  // });
+  // it("Should be able to delete inspirations", () => {
+  //   cy.visit("http://localhost:3000/inspiration")
+  //   cy.wait(500)
+  //   .get(".save-button")
+  //   .click()
+  //   .get(".see-inspo-button")
+  //   .click()
+  //   .get("button.delete-button")
+  //   .click()
+  // })
+  // it("Should be able to click the 'Get More Inspiration' button and go to Inspiration page", () => {
+  //   cy.get("button")
+  //     .click()
+  //     .url()
+  //     .should("eq", "http://localhost:3000/inspiration");
+  // });
+});
