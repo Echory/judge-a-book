544ba812ba6dd660f42f3d96827656d310ab8da5
Emma Chory <emmachory@gmail.com>
2022-03-04 19:15:15 -0700

    Style delete button and change background img

--- src/SavedPage/SavedPage.js
+++ src/SavedPage/SavedPage.js
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom"
 const SavedPage = ({savedBooks, deleteBook}) => {
   return(
     <div className="saved-container">
-      <Link to="/">
-        <button className="back-button">Back To Vote</button>
-      </Link>
-      <h1 className="to-read">To-read</h1>
+      <nav className="nav-bar">
+        <Link to="/">
+          <button className="back-button">Back To Vote</button>
+        </Link>
+        <h1 className="to-read">To-read</h1>
+      </nav>
       <section className="fetched-books">
         {savedBooks.map(({ id, volumeInfo }) => (
           <div className="book-div" key={id}>
             <p className="cover-title">{volumeInfo.title}</p>
             <img className="saved-img" src={volumeInfo.imageLinks.thumbnail}/>
-            <button onClick={() => deleteBook(id)}>Delete</button>
+            <button className="delete-button" onClick={() => deleteBook(id)}>✖️</button>
           </div>
         ))}
       </section>
